3b6d8c462ad6e03950ec915b6b53cc69273ae2e4
“Erick” <144264479+erick718@users.noreply.github.com>
2024-03-21 01:45:01 -0400

    changes

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,1 +1,8 @@
+document.getElementById("date").innerHTML = "Today is: " + Date();
 
+function welcomeUser() {
+    let userName = document.getElementById("name").value;
+    let userFeeling = document.getElementById("feeling").value;
+    let welcomeText = `Ego Shark welcomes you, ${userName}!. We're glad you are doing ${userFeeling}!`;
+    document.getElementById("welcome").innerHTML = welcomeText;
+}

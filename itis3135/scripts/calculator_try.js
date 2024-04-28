b9933c6eebbb5d166793b66558415b17334fb1f9
“Erick” <144264479+erick718@users.noreply.github.com>
2024-04-28 18:47:00 -0400

    calc try

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,54 @@
+let calcDisplay = document.getElementById('calc-display');
+let num1;
+let num2;
+let result;
+let operator;
+
+function display(value) {
+    calcDisplay.textContent += value;
+}
+
+function clearDisplay() {
+    calcDisplay.textContent = '';
+}
+
+function add() {
+    num1 = parseFloat(calcDisplay.textContent);
+    clearDisplay();
+    operator = '+';
+}
+
+function subtract() {
+    num1 = parseFloat(calcDisplay.textContent);
+    clearDisplay();
+    operator = '-';
+}
+
+function multiply() {
+    num1 = parseFloat(calcDisplay.textContent);
+    clearDisplay();
+    operator = '*';
+}
+
+function divide() {
+    num1 = parseFloat(calcDisplay.textContent);
+    clearDisplay();
+    operator = '/';
+}
+
+function calculate() {
+    num2 = parseFloat(calcDisplay.textContent);
+    if (operator === '+'){
+        result = num1 + num2;
+    }
+    else if (operator === '-') {
+        result = num1 - num2;
+    }
+    else if (operator === '*') {
+        result = num1 * num2;
+    }
+    else if (operator === '/') {
+        result = num1 / num2;
+    }
+    calcDisplay.textContent = result;
+}
